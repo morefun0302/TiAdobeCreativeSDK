ca3b31440a0f977a1c257345231144b5002deefb
Jerome Danthinne <jerome@grincheux.be>
2015-03-01 20:31:22 +0100

    Added "animated" option as an argument.

--- iphone/Classes/ComKossoTiadobecreativesdkModule.m
+++ iphone/Classes/ComKossoTiadobecreativesdkModule.m
@@ -129,7 +129,7 @@
     return tools;
 }
 
--(void)newEditorController:(UIImage *)source withTools:(NSArray *)toolKey
+-(void)newEditorController:(UIImage *)source withTools:(NSArray *)toolKey animated:(BOOL)animated
 {
     
     NSArray *tools = [self convertToRealToolsKey:toolKey];
@@ -139,8 +139,8 @@
     [AdobeImageEditorCustomization setToolOrder:tools];
     
     [editorController setDelegate:self];
-    
-    [[TiApp app] showModalController: editorController animated: NO];
+
+    [[TiApp app] showModalController: editorController animated: animated];
 }
 
 #pragma Public APIs
@@ -163,7 +163,8 @@
     
     UIImage *source = [self convertToUIImage:[params objectForKey:@"image"]];
     NSArray *tools = [NSArray arrayWithArray:(NSArray *)[params objectForKey:@"tools"]];
-    [self newEditorController:source withTools:tools];
+    BOOL animated = [TiUtils boolValue:@"animated" properties:params def:NO];
+    [self newEditorController:source withTools:tools animated:animated];
 }
 
 #define view_parentViewController(_view_) (([_view_ parentViewController] != nil || ![_view_ respondsToSelector:@selector(presentingViewController)]) ? [_view_ parentViewController] : [_view_ presentingViewController])
